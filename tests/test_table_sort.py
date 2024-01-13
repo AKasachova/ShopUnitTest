b854af30d96619de17d9b036ca7cdf882cbdf4e2
Aliona Kosachova <alionakosachova@gmail.com>
2024-01-13 14:42:37 +0200

    fixies_remove_3

--- tests/test_table_sort.py
+++ tests/test_table_sort.py
@@ -14,11 +14,7 @@ def browser():
     driver.quit()
 
 
-<<<<<<< HEAD
-def test_table_sort(browser, age_to_filter_1=30, salary_to_filter_1=675000):
-=======
 def test_table_sort(browser, age_to_filter_1=30, salary_to_filter_1=675000.0):
->>>>>>> 6cea9ec2792a8e04e4c96d2a67b0f9f54737259f
     filtered_employees = get_filtered_employees(browser, age_to_filter_1, salary_to_filter_1)
     for employee in filtered_employees:
         assert employee.age > age_to_filter_1 and employee.salary <= salary_to_filter_1, f"Employee doesn't much needed salary or age, the Employee age: {employee.age}, the Employee salary: {employee.salary}"
