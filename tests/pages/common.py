9eca1fedf3901fc37973e9c114ddd97d23732744
Aliona Kosachova <alionakosachova@gmail.com>
2024-01-14 10:55:33 +0200

    task_70_in_progress

---
+++ tests/pages/common.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class CommonOps:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self._wait = WebDriverWait(self.driver, 10)
+
+    def wait_for(self, locator):
+        return self._wait.until(ec.presence_of_element_located(locator))
+
+    def find(self, locator):
+        return self.driver.find_element(*locator)
+
+    def scroll_to_element(self, locator):
+        actions = ActionChains(self.driver)
+        actions.move_to_element(self.find(locator))
+        actions.perform()
---
+++ tests/test_log_in.py
@@ -0,0 +1,14 @@
+from .pages.formLogin import LogIn
+from .pages.logout import Logout
+
+
+def test_log_in_success(driver):
+    form = LogIn(driver)
+    form.navigate_to_form()
+    form.enter_login_username("student")
+    form.enter_login_password("Password123")
+    form.click_login_button()
+
+    logout_page = Logout(driver)
+    success_log_in_text = logout_page.successful_log_in()
+    assert "Logged In Successfully" in success_log_in_text, "User isn't logged in!"
