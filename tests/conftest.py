94299a71f42eef4a79c4727f60e282f5f0aec848
Aliona Kosachova <alionakosachova@gmail.com>
2024-01-25 15:38:26 +0200

    task_110_in_progress

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,25 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    # driver = webdriver.Firefox()
+    moon_options = {
+         "enableVNC": True,
+         "enableVideo": False
+     }
+
+    options = Options()
+    options.set_capability("browserName", "chrome")
+    options.set_capability("browserVersion", "119.0")
+    options.set_capability("moon:options", moon_options)
+
+    driver = webdriver.Remote(
+         command_executor="http://localhost:4444/wd/hub",
+         options=options)
     driver.implicitly_wait(5)
     driver.get("https://practicetestautomation.com/practice-test-login/")
     driver.maximize_window()
-    yield driver
-    driver.quit()
-
+    return driver
+    # driver.quit()
